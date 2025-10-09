269a524562e4c27191de3c2b98dadd0500ff7750
orkvacy <krotago@gmail.com>
2025-10-10 03:20:24 +0800

    add pertemuan 5

---
+++ pertemuan-5/pertemuan5.py
@@ -0,0 +1,64 @@
+praktikum = ['APD', 'orsikom', 'jarkom', 'RPL']
+
+
+# praktikum.remove('APD')
+# del praktikum[2]
+# buang = praktikum.pop()
+
+# print(praktikum)
+
+# 004
+# 013
+# 007
+# angka = [1, 2, 3 ,4 ,5 ,6 ,7 ,8 ,9 , 10, 11]
+# huruf = ['a', 'b', 'c']
+# kombinasi =  huruf * 3
+# print(kombinasi)
+
+
+#12, 21
+
+# praktikum.append(inputNilai) #017
+
+# praktikum[2] = APD
+# praktikum.insert(3, 'basdat')
+
+# print(praktikum[1][1])#03
+
+
+# print(praktikum)
+# for i in praktikum:
+#     print(i)
+
+
+
+
+# mahasiswa = [
+#                 ["Daffa", "Dante", "Santoso"], ["Pernanda", "Triya", "Ahnaf"], 
+#                 ['yoga', 'denny']
+#             ]
+# # perulangan for untuk mendapatkan semua elemen
+# for i in mahasiswa:
+#     for j in i :
+#         print (j)
+#         #[1][2]
+
+
+
+# aslab = ['harahap', 'deny']
+# matkul = ('apd', 'matdis') #013
+# listmatkul = list(matkul)
+# kombinasi = aslab + listmatkul
+# print(kombinasi)
+# print(tuple)
+# for i in matkul:
+#     print(i)
+
+# matkul.append('kalkulus')
+# print(matkul)
+
+
+tugas = ('yoga', 'arduino','scrapping', 'matriks', 'visualisasi')
+# beri key setiap values
+(nama, orsikom, *manipulasiData) = tugas
+print(manipulasiData) #016
